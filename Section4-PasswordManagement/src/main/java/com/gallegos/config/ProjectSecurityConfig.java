3348c7ade824770e15bfebc17190e7f1df0dc66b
David Gallegos <dgallegos1985@gmail.com>
2024-07-02 17:53:47 +0200

    Password management with passwordEncoders section

---
+++ Section4-PasswordManagement/src/main/java/com/gallegos/config/ProjectSecurityConfig.java
@@ -0,0 +1,44 @@
+package com.gallegos.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class ProjectSecurityConfig {
+
+    /*
+    * By Default, Sprint Security secure all the Rest calls, so we will define the calls to secure
+    * Secure -> myAccount, myBalance, myLoans, myCards
+    * No Secure -> contact, notices
+     */
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeHttpRequests(requests -> {
+            ((AuthorizeHttpRequestsConfigurer.AuthorizedUrl)
+                    requests.requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards"))
+                    .authenticated();
+            ((AuthorizeHttpRequestsConfigurer.AuthorizedUrl)
+                    requests.requestMatchers("/notices", "/contact", "/register"))
+                    .permitAll();
+        });
+        http.formLogin(Customizer.withDefaults());
+        http.httpBasic(Customizer.withDefaults());
+        return (SecurityFilterChain)http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
